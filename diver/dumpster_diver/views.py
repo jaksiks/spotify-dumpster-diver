19ba8a7bd9889a830214eec43d7cfabe25fb08cb
markunas <phil.markunas@gmail.com>
2023-03-19 22:15:19 -0400

    Imports SpotifyWrapper to views

--- diver/dumpster_diver/views.py
+++ diver/dumpster_diver/views.py
@@ -1,16 +1,20 @@
+from spotify.spotify_wrapper import SpotifyWrapper
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
 
+    print('Yo this worked')
+    
     ## Call your data processing functions here
     ## This is what gets called when a user hits the website index/root directory "/"
 
-
+    test_var = [1,2,3]
     ## Then pass your processed data to the frontend via "context" below
     context = {
         ## Put data here that you want to pass to the frontend in key-value pair/dictionary form:
         ## 'key':variable,
+        'test_list':test_var
     }
     
     return render(request, 'dumpster_diver/index.html', context)
